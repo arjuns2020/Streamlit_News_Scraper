5c76c5452abbe49b602bac0d03ce9786309d44b6
Arjun <arjunpks95@gmail.com>
2023-12-19 16:05:15 -0800

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ if st.sidebar.button("Search"):
 
         # Display results
         st.subheader("Results")
-        st.subheader("Click "Download Filtered Results" button adjacent to the search button")
+    
         st.dataframe(df)
 
         # Download button
